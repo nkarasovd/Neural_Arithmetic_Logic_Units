a64f52a403e5e2517c33db2f098773f41c71e206
Nikolai <nikolai.karasoff@yandex.ru>
2021-05-25 13:38:51 +0300

    rewrite mlp model

--- src/models/mlp.py
+++ src/models/mlp.py
@@ -1,37 +1,23 @@
-import torch
 import torch.nn as nn
 
 from typing import List, Union
 
 from utils import string2function, activation_functions
 
+from models.model import GeneralModel
 
-class MLP(nn.Module):
+
+class MLP(GeneralModel):
     def __init__(self, input_dim: int, output_dim: int,
                  num_hidden_layers: int, hidden_dim: int, activation: str = 'relu'):
-        super().__init__()
-        self.num_hidden_layers = num_hidden_layers
-        self.input_dim = input_dim
-        self.output_dim = output_dim
-        self.hidden_dim = hidden_dim
-        self.activation = string2function(activation)()
-
+        super().__init__(input_dim, output_dim,
+                         num_hidden_layers, hidden_dim)
+        self.activation = string2function(activation)
         self.model = self.build_model()
 
     def layer(self, input_dim: int, output_dim: int) -> \
             List[Union[nn.Linear, activation_functions]]:
-        return [nn.Linear(input_dim, output_dim),
-                self.activation]
+        if output_dim != self.hidden_dim or self.activation is None:
+            return [nn.Linear(input_dim, output_dim)]
 
-    def build_model(self) -> nn.Sequential:
-        layers = self.layer(self.input_dim, self.hidden_dim)
-
-        for _ in range(self.num_hidden_layers):
-            layers += self.layer(self.hidden_dim, self.hidden_dim)
-
-        layers += [nn.Linear(self.hidden_dim, self.output_dim)]
-
-        return nn.Sequential(*layers)
-
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        return self.model(x)
+        return [nn.Linear(input_dim, output_dim), self.activation()]
