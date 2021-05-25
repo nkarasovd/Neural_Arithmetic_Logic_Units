5f1c89fb64dbd4b8a7e56fe173bc620af36f6584
Nikolai <nikolai.karasoff@yandex.ru>
2021-05-25 13:40:01 +0300

    add NAC model

---
+++ src/models/nac.py
@@ -0,0 +1,34 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from torch.nn.parameter import Parameter
+
+from typing import List
+
+from models.model import GeneralModel
+
+
+class NACCell(nn.Module):
+    def __init__(self, input_dim: int, output_dim: int):
+        super().__init__()
+        self.W_hat = Parameter(torch.Tensor(output_dim, input_dim))
+        self.M_hat = Parameter(torch.Tensor(output_dim, input_dim))
+
+        self.register_parameter('W_hat', self.W_hat)
+        self.register_parameter('M_hat', self.M_hat)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        W = torch.tanh(self.W_hat) * torch.sigmoid(self.M_hat)
+        return F.linear(x, W, bias=None)
+
+
+class NAC(GeneralModel):
+    def __init__(self, input_dim: int, output_dim: int,
+                 num_hidden_layers: int, hidden_dim: int):
+        super().__init__(input_dim, output_dim,
+                         num_hidden_layers, hidden_dim)
+        self.model = self.build_model()
+
+    def layer(self, input_dim: int, output_dim: int) -> List[NACCell]:
+        return [NACCell(input_dim, output_dim)]
